0dfdc9e6a565a0af6136e2fc5d758d697236d0af
Mikhail Abramishvili <mg@abramishvili.net>
2022-09-10 15:24:58 +0500

    .

---
+++ database/migrations/2022_09_10_101511_create_banners_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->integer('page_id')->nullable();
+            $table->integer('point_id')->nullable();
+            $table->string('name');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+};
---
+++ database/migrations/2022_09_10_101713_create_points_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('scheme1_id');
+            $table->integer('scheme2_id')->nullable();
+            $table->string('floor1_text_begin')->nullable();
+            $table->string('floor1_text_end')->nullable();
+            $table->string('floor2_text_begin')->nullable();
+            $table->string('floor2_text_end')->nullable();
+            $table->text('route_code_floor1');
+            $table->text('route_code_floor2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+};
