8911a4aa78951e0e19cff8f45533d6fa010185b4
Mikhail Abramishvili <mg@abramishvili.net>
2022-09-11 16:29:56 +0500

    .

--- resources/js/components/front/App.vue
+++ resources/js/components/front/App.vue
@@ -1,5 +1,48 @@
 <template>
     <div class="wrapper">
-        <router-view></router-view>
+        <PesokTheme
+            v-if="theme == 'pesok'"
+            ref="pesok" />
     </div>
 </template>
+
+<script>
+    import PesokTheme from './pesok/layout.vue'
+
+    export default {
+        data() {
+            return {
+                settings: {},
+
+                theme: 'pesok'
+            }
+        },
+        created() {
+            // this.loadSettings()
+        },
+        methods: {
+            loadSettings() {
+                axios.get(`/api/settings`)
+                .then(response => {
+                    this.settings = response.data
+                    
+                })
+            },
+        },
+        mounted() {
+            this._keyListener = function(e) {
+                if(e.key === 'q' && (e.ctrlKey || e.metaKey) || e.key === 'й' && (e.ctrlKey || e.metaKey)) {
+                    e.preventDefault()
+                    window.location.href = '/admin'
+                }
+            }
+            document.addEventListener('keydown', this._keyListener.bind(this))
+        },
+        beforeMount() {
+            // document.oncontextmenu = new Function("return false")
+        },
+        components: {
+            PesokTheme,
+        }
+    }
+</script>
---
+++ resources/js/components/front/pesok/layout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pesok-layout">
+        {{theme}}
+        <router-view v-slot="{ Component }">
+            <keep-alive>
+                <component :is="Component" />
+            </keep-alive>
+        </router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                //
+            }
+        },
+        computed: {
+            theme() {
+                return this.$root.theme
+            }
+        },
+        created() {
+            //
+        },
+    }
+</script>
+
+<style>
+@import "../../../../css/pesok/vertical.css"
+</style>
