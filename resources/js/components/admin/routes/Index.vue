3642e39346887a80263bf76a7919b77b16b50bd6
Mikhail Abramishvili <mg@abramishvili.net>
2022-09-20 11:31:51 +0500

    .

--- resources/js/components/admin/routes/Index.vue
+++ resources/js/components/admin/routes/Index.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <div class="content-header px-4">
-            <h1>Маршруты</h1>
+            <div class="d-flex">
+                <h1 class="me-2">Маршруты</h1>
+
+                <select v-if="kiosks.length" v-model="selected.kiosk" @change="changeKiosk()" class="form-select">
+                    <option v-for="kiosk in kiosks" :value="kiosk.id">{{ kiosk.name }}</option>
+                </select>
+            </div>
             <router-link :to="{name: 'RouteMaster'}" class="btn btn-primary">Добавить</router-link>
         </div>
-
+        
         <div v-if="views.loading" class="p-4">
             <Loader />
         </div>
@@ -31,8 +37,11 @@ export default {
     data() {
         return {
             routes: [],
-
             kiosks: [],
+
+            selected: {
+                kiosk: null,
+            },
 
             views: {
                 loading: true,
@@ -40,9 +49,18 @@ export default {
         }
     },
     created() {
+        this.loadKiosks()
         this.loadRoutes()
     },
     methods: {
+        loadKiosks() {
+            axios.get(`/api/admin/kiosks`)
+            .then(response => {
+                this.kiosks = response.data
+
+                this.selected.kiosk = this.$route.params.kiosk
+            })
+        },
         loadRoutes() {
             axios.get(`/api/admin/routes/${this.$route.params.kiosk}`)
             .then(response => {
@@ -50,6 +68,13 @@ export default {
 
                 this.views.loading = false
             })
+        },
+        changeKiosk() {
+            this.$router.push({ name: 'Routes', params: {kiosk: this.selected.kiosk} })
+            
+            setTimeout(() => {
+                this.loadRoutes()
+            }, 100)
         },
     },
 }
