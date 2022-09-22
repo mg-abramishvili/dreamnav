12293e4ab4d892aa9f6f0649d0d120fca3e94274
Mikhail Abramishvili <mg@abramishvili.net>
2022-09-22 18:52:23 +0500

    .

--- database/seeders/ScreensaverSeeder.php
+++ database/seeders/ScreensaverSeeder.php
@@ -12,15 +12,28 @@ class ScreensaverSeeder extends Seeder
     public function run()
     {
         DB::table('screensavers')->insert([
-            'id' => 1,
-            'page_id' => null,
-            'point_id' => null,
-            'event_id' => null,
-            'name' => 'Заставка №1',
-            'type' => 'image',
-            'content' => '/img/pesok/screensaver1.jpg',
-            'created_at' => '2022-09-10 07:00:00',
-            'updated_at' => '2022-09-10 07:00:00'
+            [
+                'id' => 1,
+                'page_id' => null,
+                'point_id' => null,
+                'event_id' => null,
+                'name' => 'Заставка 1 - Фото',
+                'type' => 'image',
+                'content' => '/img/pesok/screensaver1.jpg',
+                'created_at' => '2022-09-10 07:00:00',
+                'updated_at' => '2022-09-10 07:00:00'
+            ],
+            [
+                'id' => 2,
+                'page_id' => null,
+                'point_id' => null,
+                'event_id' => null,
+                'name' => 'Заставка 2 - Видео с коптера',
+                'type' => 'video',
+                'content' => '/img/pesok/screensaver2.mp4',
+                'created_at' => '2022-09-10 07:00:00',
+                'updated_at' => '2022-09-10 07:00:00'
+            ],
         ]);
     }
 }
